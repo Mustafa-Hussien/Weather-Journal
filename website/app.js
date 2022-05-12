986afa0a9b71aa444e31cc76d349c1b3d5b80492
Mustafa Hussien <mustafa.hussien100@gmail.com>
2022-05-12 02:53:36 +0200

    added async function and event listener

--- website/app.js
+++ website/app.js
@@ -1,4 +1,17 @@
 /* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=46ff55eb47c0ac027779dcdff5dd1a67'
+const zipCode =  document.getElementById('zip').value;
+
+// Create async function 
+const getWeatherData = async (url,zip, key) => {
+    const res = await fetch(url+zip+key)
+}
+
+// Event listener for the generate button
+document.getElementById('generate').addEventListener('click', (e) => {
+    getWeatherData(baseURL, zipCode,apiKey)
+})
 
 // Create a new date instance dynamically with JS
 let d = new Date();
